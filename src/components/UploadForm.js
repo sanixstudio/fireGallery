0c18a7b93672d5722127decbd5d96a44375c6146
Adnan Niaz <adnanniaz77@yahoo.com>
2020-07-30 14:20:17 -0700

    created functionality for UploadForm with error handling

--- src/components/UploadForm.js
+++ src/components/UploadForm.js
@@ -1,1 +1,30 @@
+import React, { useState } from "react";
 
+export default function UploadForm() {
+    const [file, setFile] = useState(null);
+    const [error, setError] = useState(null);
+
+    const types = ["image/png", "image/jpeg"];
+
+    const handleChange = (e) => {
+        let selected = e.target.files[0];
+
+        if (selected && types.includes(selected.type)) {
+            setFile(selected);
+            setError("");
+        } else {
+            setFile(null);
+            setError("Please select an image file (png or jpeg)");
+        }
+    };
+
+    return (
+        <form>
+            <input type="file" onChange={handleChange} />
+            <div className="output">
+                {error && <div className="error">{error}</div>}
+                {file && <div>{file.name}</div>}
+            </div>
+        </form>
+    );
+}
