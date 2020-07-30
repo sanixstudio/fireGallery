51693ab02119c9619365466c1ea4f6b44bcc77a1
Adnan Niaz <adnanniaz77@yahoo.com>
2020-07-30 14:09:34 -0700

    added firebase configuration

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
-import Title from "./components/Title/Title";
+import Title from "./components/Title";
+import UploadForm from "./components/UploadForm";
 
 function App() {
     return (
         <div className="App">
             <Title />
+            <UploadForm />
         </div>
     );
 }
---
+++ src/firebase/config.js
@@ -0,0 +1,21 @@
+import * as firebase from "firebase";
+import "firebase/storage";
+import "firebase/firestore";
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyDDnays3Ux-_0uiQzD1sLCB1chzmuVjBEs",
+    authDomain: "firegallery-1dac4.firebaseapp.com",
+    databaseURL: "https://firegallery-1dac4.firebaseio.com",
+    projectId: "firegallery-1dac4",
+    storageBucket: "firegallery-1dac4.appspot.com",
+    messagingSenderId: "835478512168",
+    appId: "1:835478512168:web:b7fb74c47de867b1d7cb1d",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const projectStorage = firebase.storage();
+const projectFirestore = firebase.firestore();
+
+export { projectStorage, projectFirestore };
